58ee1edff84fb2a140a0d502f63f4d8c2d5a56c6
eswaribala <parameswaribala@gmail.com>
2022-11-29 12:38:56 +0530

    updated

---
+++ customerapi/customerapi/src/main/java/com/boa/customerapi/services/IndividualService.java
@@ -0,0 +1,63 @@
+package com.boa.customerapi.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.boa.customerapi.facades.IndividualFacade;
+import com.boa.customerapi.models.Individual;
+import com.boa.customerapi.repositories.IndividualRepository;
+
+@Service
+public class IndividualService implements IndividualFacade {
+    @Autowired
+	private IndividualRepository individualRepository;
+	@Override
+	public Individual addIndividual(Individual individual) {
+		// TODO Auto-generated method stub
+		return this.individualRepository.save(individual);
+	}
+
+	@Override
+	public List<Individual> getAllIndividuals() {
+		// TODO Auto-generated method stub
+		return this.individualRepository.findAll();
+	}
+
+	@Override
+	public Individual getIndividualById(long customerId) {
+		// TODO Auto-generated method stub
+		return this.individualRepository.findById(customerId).orElse(null);
+	}
+
+	@Override
+	public Individual updateIndividual(long customerId, long contactNo, String email, String password) {
+		// TODO Auto-generated method stub
+	
+	   Individual individual= this.getIndividualById(customerId);
+	   if(individual !=null) {
+		   if(contactNo>0)
+		   individual.setContactNo(contactNo);
+		   if(email!=null && email.length()>0)
+		   individual.setEmail(email);
+		   if(password!=null && password.length()>0)
+		   individual.setPassword(password);
+		   return this.individualRepository.save(individual);
+	   }
+	   else
+		   return null;
+	}
+
+	@Override
+	public boolean deleteIndividual(long customerId) {
+		boolean status=true;
+		// TODO Auto-generated method stub
+		this.individualRepository.deleteById(customerId);
+		  Individual individual= this.getIndividualById(customerId);
+		  if(individual !=null)
+			  status=false;
+		  return status;
+	}
+
+}
