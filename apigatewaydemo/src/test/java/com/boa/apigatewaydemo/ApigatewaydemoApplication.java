91780e4e819c83db4ebb6b0585a980704898a419
eswaribala <parameswaribala@gmail.com>
2022-12-01 12:45:00 +0530

    updated

---
+++ apigatewaydemo/src/test/java/com/boa/apigatewaydemo/ApigatewaydemoApplication.java
@@ -0,0 +1,16 @@
+package com.boa.apigatewaydemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+
+@SpringBootApplication
+public class ApigatewaydemoApplication {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SpringApplication.run(ApigatewaydemoApplication.class, args);
+	}
+
+}
