12adb44ba10397685e7a0bf2c02e2e57f1c121f3
eswaribala <parameswaribala@gmail.com>
2022-12-02 10:22:35 +0530

    updated

--- apigatewaydemo/src/main/java/com/boa/apigatewaydemo/ApigatewaydemoApplication.java
+++ apigatewaydemo/src/main/java/com/boa/apigatewaydemo/ApigatewaydemoApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 
 
@@ -17,4 +19,8 @@ public class ApigatewaydemoApplication {
 		SpringApplication.run(ApigatewaydemoApplication.class, args);
 	}
 
+	@Bean
+	public RestTemplate getRestTemplate() {
+		return new RestTemplate();
+	}
 }
